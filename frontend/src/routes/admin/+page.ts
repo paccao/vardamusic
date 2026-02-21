d1198bd592f221b61ec625e0b59223d25a6d771c
paccao <16489683+paccao@users.noreply.github.com>
2026-02-21 14:09:44 +0100

    Remove redundant stuff

--- frontend/src/routes/admin/+page.ts
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit'
-
-// TODO: Replace this check and make the admin routes into properly auth-protected routes
-export const load = async () => {
-  if (!import.meta.env.DEV) {
-    error(404)
-  }
-}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,14 @@ import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
 import { keystatic } from 'keystatic-sveltekit'
-console.log('VITE CONFIG CWD:', process.cwd())
-console.log('VITE CONFIG __dirname:', __dirname)
 
 export default defineConfig({
-  plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    // keystatic(),
+    enhancedImages(),
+    sveltekit(),
+  ],
   // Ensure the SvelteKit app is running on 127.0.0.1 to be consistent with Keystatic
   // For production, we want the host to be determined by the runtime instead
   server:
